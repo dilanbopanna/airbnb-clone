ffc17249f35030fe7c6fd6e4fa7cb1d5da6db9fd
Bopanna MJ <bopanna.mj@juspay.in>
2023-03-20 14:08:10 +0530

    removed tailwind config from gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const plugin = require("tailwindcss/plugin");
+module.exports = {
+    darkMode: "class",
+    content: ["./src/**/*.js"],
+    theme: {
+        extend: {},
+        fontFamily: {
+            "fig-tree": '"FigTree"',
+        },
+    },
+    plugins: [
+        plugin(function ({ addUtilities }) {
+            const newUtilities = {
+                ".no-scrollbar::-webkit-scrollbar": {
+                    display: "none",
+                },
+                ".show-scrollbar::-webkit-scrollbar": {
+                    overflow: "scroll",
+                    height: "4px",
+                    width: "4px",
+                },
+                ".show-scrollbar::-webkit-scrollbar-thumb": {
+                    borderRadius: "2px",
+                    backgroundColor: "#9A9FA8",
+                },
+            };
+            addUtilities(newUtilities);
+        }),
+    ],
+}
